77d3f3c2c83e01bd6e51acefeb1b11c213e94f93
Aslıhan Kocabaş <kocabasasli66@gmail.com>
2020-10-05 00:15:30 +0300

    add method added

---
+++ src/com/aslihankocabas/Arcade/Intro/TheJourneyBegins/Add.java
@@ -0,0 +1,13 @@
+package com.aslihankocabas.Arcade.Intro.TheJourneyBegins;
+
+public class Add {
+    public static void main(String[] args) {
+        Add testAdd = new Add();
+        System.out.println("Input: 2, -39\nExpected Output: -37\nOutput: " + testAdd.add(2, -39));
+    }
+
+    //sum of two numbers
+    int add(int param1, int param2) {
+        return param1 + param2;
+    }
+}
